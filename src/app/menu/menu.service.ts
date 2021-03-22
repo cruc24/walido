0e8a2886ff2717107940ec3264be08f7055f84d8
Luca <cruc24@gmail.com>
2021-03-22 22:13:50 +0100

    22/03 22:13

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -1,3 +1,4 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Piatto } from '../models/piatto.model';
 
@@ -5,7 +6,7 @@ import { Piatto } from '../models/piatto.model';
   providedIn: 'root',
 })
 export class MenuService {
-  constructor() {}
+  constructor(private http : HttpClient) {}
 
   menu: Piatto[] = [
     {
@@ -31,4 +32,9 @@ export class MenuService {
       imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR6jYE2QLe-hIQ2ITfc8zF0j6g7gD9nOO4TQ&usqp=CAU',
     },
   ];
+
+
+  getMenu(){
+    return this.http.get("URL chiamata backend GET");
+  }
 }
