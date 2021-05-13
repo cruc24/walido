7a818a8d0cc7686b2e0571a9d1211a64c2fac813
Luca Cruciata <cruc24@gmail.com>
2021-05-13 21:31:25 +0200

    13/05 21.31

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,16 +15,19 @@ const routes: Routes = [
       path:'',
       loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
       },
-      {
-        path: 'order',
-        loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
-      },
-      {
-        path: 'menu',
-        loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
-      },
+      // {
+      //   path: 'order',
+      //   loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+      // },
+      // {
+      //   path: 'menu',
+      //   loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+      // },
+      // {
+      //   path: 'intro',
+      //   loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+      // }
     ]
-
   },
 
 
